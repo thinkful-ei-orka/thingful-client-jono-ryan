e1d0535d3b9d765d07c0b2e30599384e390b844d
Jon <jdog992@gmail.com>
2020-06-18 01:35:04 -0400

    auth

--- src/services/thing-api-service.js
+++ src/services/thing-api-service.js
@@ -1,9 +1,11 @@
+import TokenService from '../services/token-service'
 import config from '../config'
 
 const ThingApiService = {
   getThings() {
     return fetch(`${config.API_ENDPOINT}/things`, {
       headers: {
+        'authorization': `basic ${TokenService.getAuthToken()}`,
       },
     })
       .then(res =>
@@ -15,6 +17,7 @@ const ThingApiService = {
   getThing(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}`, {
       headers: {
+        'authorization': `basic ${TokenService.getAuthToken()}`,
       },
     })
       .then(res =>
@@ -26,6 +29,7 @@ const ThingApiService = {
   getThingReviews(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
       headers: {
+        'authorization': `basic ${TokenService.getAuthToken()}`,
       },
     })
       .then(res =>
